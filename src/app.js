cef6fb5b92db6c0331439f38285d5c053f56c851
Prajyot711 <cprajyot43@gmail.com>
2026-07-31 17:58:55 +0530

    multiple route handlers and next method

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 
  const app = express();
 
- app.get("/users/:userId/:name/:password",(req,res)=>{
-   console.log({...req.params})
-   res.send({'firstName':"Prajyot",'lastName':"Chavan"})
- })
+//  app.get("/users/:userId/:name/:password",(req,res)=>{
+//    console.log({...req.params})
+//    res.send({'firstName':"Prajyot",'lastName':"Chavan"})
+//  })
+
+ app.use("/test",
+  (req,res,next) => {
+    console.log("res 1");
+    //res.send("res1");
+    next()
+  },
+  (req,res,next) => {
+    console.log("res 2");
+    res.send("res2");
+  }
+
+)
 
  app.listen(3000, () => {
     console.log("server")
